73ddd755df1518d6803a9cc057e61fd4394631f8
luchkam <sluchin@mail.ru>
2025-04-24 10:18:16 +0300

    Update server.js 1018

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 // Новый endpoint для создания потока
 app.get('/new-thread', async (req, res) => {
   try {
